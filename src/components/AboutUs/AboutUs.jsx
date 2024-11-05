b49692c463b5a5389b61aa4d09b2fdd3c965727d
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-05 16:03:44 +0600

    carosel addeed done

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,31 @@
-
 function AboutUs() {
-    return (
-        <div>
-            this is a about Us Section container
+  return (
+    <div>
+      <div
+        className="hero min-h-[600px] mt-0 mb-14 rounded-2xl"
+        style={{
+          backgroundImage:
+            "url(https://cdn.pixabay.com/photo/2016/11/29/02/07/drone-1866742_960_720.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60 rounded-2xl"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="">
+            <h1 className="mb-5 text-5xl font-bold text-purple-200">Who we are ??</h1>
+            <p className="text-5xl font-bol mb-5">
+              Gives you quality services at the best prices.
+            </p>
+            <p className="text-xl fonr-semibold mb-4 w-6/12 mx-auto">
+              Dignissim dictumst curae nunc lectus leo pretium rutrum. Mauris
+              class suscipit dictumst est praesent letius integer fusce
+              convallis si. Conubia integer senectus duis blandit lectus nec.
+            </p>
+            <button className="btn bg-purple-600 text-gray-100 font-semibold hover:bg-purple-700">Send Massage</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-dupe-keys */
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui"
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     daisyui,
+    require('flowbite/plugin')
+
   ],
 }
