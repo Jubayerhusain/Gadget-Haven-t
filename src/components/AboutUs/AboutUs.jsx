93916a9f4066a88ff59cba357c95987cb39d03c4
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-05 17:30:00 +0600

    why You Buy section  done

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,7 +3,12 @@ import man1 from "./../../assets/man1.jpg";
 import man2 from "./../../assets/man2.jpg";
 import man3 from "./../../assets/man3.jpg";
 import man4 from "./../../assets/man4.jpg";
-import { hideElement } from './../../../node_modules/flowbite-datepicker/js/lib/dom';
+import { SiShopee } from "react-icons/si";
+import { AiOutlineProduct } from "react-icons/ai";
+import { GrAnnounce } from "react-icons/gr";
+import { FaShippingFast } from "react-icons/fa";
+import { GrSecure } from "react-icons/gr";
+
 function AboutUs() {
   return (
     <div>
@@ -152,11 +157,13 @@ function AboutUs() {
         </div>
       </div>
       {/* meet out team section  */}
-      <div className="mt-28"> 
-        <h1 className="text-5xl font-bold text-purple-700 text-center pt-8">Meet Out Team </h1>
+      <div className="mt-28">
+        <h1 className="text-5xl font-bold text-purple-700 text-center pt-8">
+          Meet Out Team{" "}
+        </h1>
       </div>
-      <div className="border-b-2 border-dashed border-gray-400 my-14" ></div>
-      <div className="flex justify-around  my-14">
+      <div className="border-b-2 border-dashed border-gray-400 my-14"></div>
+      <div className="flex justify-around  my-20">
         <div className="card card-compact bg-base-100 w-80 shadow-xl">
           <div className="avatar flex justify-center">
             <div className="ring-primary ring-offset-base-100 w-40 rounded-full ring ring-offset-2">
@@ -180,7 +187,7 @@ function AboutUs() {
             <h2 className="font-bold text-2xl text-gray-700 text-center my-2">
               Freddie Scott
             </h2>
-            <p className="text-center text-md font-semibold">Co-Founder</p> 
+            <p className="text-center text-md font-semibold">Co-Founder</p>
           </div>
         </div>
         <div className="card card-compact bg-base-100 w-80 shadow-xl">
@@ -191,9 +198,11 @@ function AboutUs() {
           </div>
           <div className="card-body">
             <h2 className="font-bold text-2xl text-gray-700 text-center my-2">
-            Jackson Hussain
+              Jackson Hussain
             </h2>
-            <p className="text-center text-md font-semibold">Business Manager</p>
+            <p className="text-center text-md font-semibold">
+              Business Manager
+            </p>
           </div>
         </div>
         <div className="card card-compact bg-base-100 w-80 shadow-xl">
@@ -204,9 +213,78 @@ function AboutUs() {
           </div>
           <div className="card-body">
             <h2 className="font-bold text-2xl text-gray-700 text-center my-2">
-            Peter Stevens
+              Peter Stevens
             </h2>
-            <p className="text-center text-md font-semibold">Marketing Manager</p>
+            <p className="text-center text-md font-semibold">
+              Marketing Manager
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* why You Buy section  */}
+      <div>
+        <div className="text-5xl font-bold text-purple-700 text-center my-20 flex space-x-2 w-3/12 mx-auto">
+          <h1>Why You Buy!</h1>
+          <p>
+            <SiShopee />
+          </p>
+        </div>
+        <div className="flex justify-between items-center mb-24">
+          {/* cart 01  */}
+          <div className="rounded-xl shadow-xl p-8 flex space-x-3">
+            <p className="text-5xl font-bold text-purple-700">
+              <AiOutlineProduct />
+            </p>
+            <div>
+              <h1 className="font-bold text-2xl text-gray-700 text-start">
+                Original Product
+              </h1>
+              <p className="text-start text-md font-semibold my-2">
+                100% Original product that covered warranty by the vendor.
+              </p>
+            </div>
+          </div>
+          {/* cart 02  */}
+          <div className="rounded-xl shadow-xl p-8 flex space-x-3">
+            <p className="text-5xl font-bold text-purple-700">
+              <GrAnnounce />
+            </p>
+            <div>
+              <h1 className="font-bold text-2xl text-gray-700 text-start">
+              30 Days Warranty
+              </h1>
+              <p className="text-start text-md font-semibold my-2">
+              You have the right to return your orders within 30 days.
+              </p>
+            </div>
+          </div>
+          {/* cart 03  */}
+          <div className="rounded-xl shadow-xl p-8 flex space-x-3">
+            <p className="text-5xl font-bold text-purple-700">
+              <FaShippingFast />
+            </p>
+            <div>
+              <h1 className="font-bold text-2xl text-gray-700 text-start">
+              Global Shipping
+              </h1>
+              <p className="text-start text-md font-semibold my-2">
+              Your orders are shipped seamlessly between countries.
+              </p>
+            </div>
+          </div>
+          {/* cart 04  */}
+          <div className="rounded-xl shadow-xl p-8 flex space-x-3">
+            <p className="text-5xl font-bold text-purple-700">
+              <GrSecure />
+            </p>
+            <div>
+              <h1 className="font-bold text-2xl text-gray-700 text-start">
+              100% Secure
+              </h1>
+              <p className="text-start text-md font-semibold my-2">
+              Your payments are secure with our private security network.
+              </p>
+            </div>
           </div>
         </div>
       </div>
