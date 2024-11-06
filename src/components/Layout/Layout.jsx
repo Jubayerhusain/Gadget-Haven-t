2a569ffc8b7c502b8a98fdd1b89cd2349af3d489
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-06 20:31:10 +0600

    title update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,41 +1,41 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import Navber from './../Header/Navber';
-import Footer from './../Footer/Footer';
-import { Toaster } from 'react-hot-toast';
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Navber from "./../Header/Navber";
+import Footer from "./../Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 function Layout() {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        const path = location.pathname;
+  useEffect(() => {
+    const path = location.pathname;
 
-        let title = "BD-TECT_GADGET"; 
-        if (path === "/") {
-            title = "Home";
-        } else if (path.includes("/product/")) {
-            title = "Product Details";
-        } else if (path === "/dashboard") {
-            title = "Dashboard";
-        } else if (path === "/aboutUs") {
-            title = "About Us";
-        } else if (path === "/statistic") {
-            title = "Statistics";
-        } else if (path.startsWith("/category")) {
-            title = "Category";
-        }
+    let title = "BD-TECT_GADGET";
+    if (path === "/") {
+      title = "Home";
+    } else if (path.includes("/product/")) {
+      title = "Product Details";
+    } else if (path === "/dashboard") {
+      title = "Dashboard";
+    } else if (path === "/aboutUs") {
+      title = "About Us";
+    } else if (path === "/statistic") {
+      title = "Statistics";
+    } else if (path.startsWith("/category")) {
+      title = "Category";
+    }
 
-        document.title ="BD-TECT_GADGET || "+ title;
-    }, [location]);
+    document.title = "BD-TECT_GADGET || " + title;
+  }, [location]);
 
-    return (
-        <div className='max-w-[1440px] mx-auto'>
-            <Toaster />
-            <Navber />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="max-w-[1440px] mx-auto">
+      <Toaster />
+      <Navber />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
