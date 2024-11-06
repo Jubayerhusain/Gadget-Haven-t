bfaa66e62f0838496463543c8f0b1aaad83635d8
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-06 20:30:33 +0600

    dynamic title with route

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,41 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navber from './../Header/Navber';
 import Footer from './../Footer/Footer';
-import{ Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
+
 function Layout() {
+    const location = useLocation();
+
+    useEffect(() => {
+        const path = location.pathname;
+
+        let title = "BD-TECT_GADGET"; // Default title
+        if (path === "/") {
+            title = "Home";
+        } else if (path.includes("/product/")) {
+            title = "Product Details";
+        } else if (path === "/dashboard") {
+            title = "Dashboard";
+        } else if (path === "/aboutUs") {
+            title = "About Us";
+        } else if (path === "/statistic") {
+            title = "Statistics";
+        } else if (path.startsWith("/category")) {
+            title = "Category";
+        }
+
+        document.title ="BD-TECT_GADGET || "+ title;
+    }, [location]);
+
     return (
         <div className='max-w-[1440px] mx-auto'>
             <Toaster />
-            <Navber></Navber>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            <Navber />
+            <Outlet />
+            <Footer />
         </div>
-    )
+    );
 }
 
-export default Layout
+export default Layout;
